c830ab946b8ba6ad1e8dcaac482edf3f11be118d
Joshua Sevy <joshuasevy@outlook.com>
2026-02-17 00:50:13 -0700

    Add server controller

--- ssl-server_student/src/main/java/com/snhu/sslserver/SslServerApplication.java
+++ ssl-server_student/src/main/java/com/snhu/sslserver/SslServerApplication.java
@@ -2,6 +2,8 @@ package com.snhu.sslserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SslServerApplication {
@@ -9,6 +11,14 @@ public class SslServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SslServerApplication.class, args);
 	}
-
 }
-//FIXME: Add route to enable check sum return of static data example:  String data = "Hello World Check Sum!";
+
+@RestController
+class ServerController{
+	@RequestMapping("/hash")
+	public String myHash(){
+		
+	   
+	    return "<p>Testing rest controller</p>";
+	}
+}
